ab4018e8dc5c2a808aea1942a97080684805dddb
Hamza Bouhouita <hamzabouhouita@gmail.com>
2023-06-20 14:47:12 +0100

    Added the files

---
+++ main.js
@@ -0,0 +1,14 @@
+                  
+// your JavaScript file
+
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
+const p =document.createElement("p")
+p.style.color="red"
+p.innerText="Hey, I'm red!"
+const body = document.querySelector('body')
+body.append(p)
